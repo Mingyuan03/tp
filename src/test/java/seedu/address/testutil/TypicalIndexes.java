16262f885ed6684510abd16c30c53130622418fd
Mingyuan03 <mingyuan.lu075500165@gmail.com>
2025-03-27 18:09:23 +0800

    Add docstring remark for TypicalIndexes and try create PR reflecting all changes from #96

--- src/test/java/seedu/address/testutil/TypicalIndexes.java
+++ src/test/java/seedu/address/testutil/TypicalIndexes.java
@@ -4,6 +4,8 @@ import seedu.address.commons.core.index.Index;
 
 /**
  * A utility class containing a list of {@code Index} objects to be used in tests.
+ * These indices SHOULD be best renamed in lieu of existence of {@code Person}, {@code Job}, and
+ * {@code Application} classes by v1.3.
  */
 public class TypicalIndexes {
     public static final Index INDEX_FIRST_PERSON = Index.fromOneBased(1);
