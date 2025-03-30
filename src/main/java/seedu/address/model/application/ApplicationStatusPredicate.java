26dc6f8f5e24af421408d426a71d7621fbfda43b
DESU CLUB <warrenlowkh@gmail.com>
2025-03-30 17:21:41 +0800

    feat: Change job view to primary view and added AppPredicate

---
+++ src/main/java/seedu/address/model/application/ApplicationStatusPredicate.java
@@ -0,0 +1,40 @@
+package seedu.address.model.application;
+
+import java.util.function.Predicate;
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Tests that an {@code Application}'s {@code ApplicationStatus} matches the target status.
+ */
+public class ApplicationStatusPredicate implements Predicate<Application> {
+    private final ApplicationStatus targetStatus;
+
+    public ApplicationStatusPredicate(ApplicationStatus targetStatus) {
+        this.targetStatus = targetStatus;
+    }
+
+    @Override
+    public boolean test(Application application) {
+        return application.getApplicationStatus().equals(targetStatus);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ApplicationStatusPredicate)) {
+            return false;
+        }
+
+        ApplicationStatusPredicate otherApplicationStatusPredicate = (ApplicationStatusPredicate) other;
+        return targetStatus.equals(otherApplicationStatusPredicate.targetStatus);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("targetStatus", targetStatus).toString();
+    }
+} 
