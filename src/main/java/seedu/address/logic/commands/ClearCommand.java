2b8ecd9d101fdd2e41616dd900d11885fbe2c753
advaypakhale <advay.pakhale@gmail.com>
2025-04-03 21:22:20 +0800

    fix: Modify clear command to clear applications

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.AddressBook;
+import seedu.address.model.ApplicationsManager;
 import seedu.address.model.Model;
 
 /**
@@ -13,11 +14,11 @@ public class ClearCommand extends Command {
     public static final String COMMAND_WORD = "clear";
     public static final String MESSAGE_SUCCESS = "Address book has been permanently cleared!";
 
-
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setAddressBook(new AddressBook());
+        model.setApplicationsManager(new ApplicationsManager());
         return CommandResult.withClearView(MESSAGE_SUCCESS);
     }
 
@@ -26,7 +27,8 @@ public class ClearCommand extends Command {
         if (other == this) {
             return true;
         }
-        // instanceof handles nulls. Simplify if-else as all ClearCommand instances lack state thus they are equal.
+        // instanceof handles nulls. Simplify if-else as all ClearCommand instances lack
+        // state thus they are equal.
         return other instanceof ClearCommand;
     }
 }
