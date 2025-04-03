f8fa42c3bd7fd7cc4a336a8aa5c5a2b9eddbdfa5
DESU-CLUB <warrenlowkh@gmail.com>
2025-04-03 18:23:13 +0800

    fix: Fix most person bugs

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -19,8 +19,8 @@ import seedu.address.model.person.Phone;
 
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
-    private static final String INVALID_PHONE = "+651234";
-    private static final String INVALID_EMAIL = "example.com";
+    private static final String INVALID_PHONE = "+651234"; // Not 8 digits 
+    private static final String INVALID_EMAIL = "example.com"; // Missing local part
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_SKILL = "#C";
 
