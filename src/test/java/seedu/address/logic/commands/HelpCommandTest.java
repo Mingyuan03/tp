e591514e58c33c023f6311071fe5f592a9426ed7
Mingyuan03 <mingyuan.lu075500165@gmail.com>
2025-04-04 08:23:48 +0800

    Add JUnits for HelpCommandTest

--- src/test/java/seedu/address/logic/commands/HelpCommandTest.java
+++ src/test/java/seedu/address/logic/commands/HelpCommandTest.java
@@ -1,9 +1,13 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.parser.CliCommands;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
@@ -15,5 +19,29 @@ public class HelpCommandTest {
     public void execute_helpCommand_success() {
         CommandResult expectedCommandResult = CommandResult.withHelp(HelpCommand.MESSAGE_SUCCESS);
         assertCommandSuccess(new HelpCommand(), this.model, expectedCommandResult, this.expectedModel);
+    }
+
+    @Test
+    public void getGeneralHelpMessage_containsAllExpectedSectionsAndCommands() {
+        String helpMessage = HelpCommand.getGeneralHelpMessage();
+        assertTrue(helpMessage.contains("Person-specific Commands below:"));
+        assertTrue(helpMessage.contains("Job-specific Commands below:"));
+        assertTrue(helpMessage.contains("Application-specific Commands below:"));
+        assertTrue(helpMessage.contains("View-toggling Commands below:"));
+        assertTrue(helpMessage.contains("Miscellaneous Commands below:"));
+
+        assertTrue(helpMessage.contains(CliCommands.ADD.getCommandWord()));
+        assertTrue(helpMessage.contains(CliCommands.ADD.getMessageUsage()));
+        assertTrue(helpMessage.contains(CliCommands.VIEWJOB.getCommandWord()));
+    }
+
+    @Test
+    public void equals() {
+        HelpCommand helpCommand = new HelpCommand();
+        // Same object type -> true. Different object type -> false.
+        assertEquals(helpCommand, helpCommand);
+        assertEquals(new HelpCommand(), helpCommand);
+        assertNotEquals(null, helpCommand);
+        assertNotEquals(new Object(), helpCommand);
     }
 }
